5afafcc252a3f3b15c4eb019e4ebae7eb9d66d71
Farhath <abdulfarhathmohd@gmail.com>
2025-04-22 15:59:03 +0530

    frontend build in backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 // Route imports
 import connectDB from './db/dbConnection.js';
@@ -11,17 +14,26 @@ import leaderboardRoutes from './routes/leaderboard.route.js';
 dotenv.config(); // Loads environment variables from .env file
 const app = express();
 
+// Emulate __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // Middleware setup
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes setup
 app.use('/api/auth', authRoute);
 app.use('/api/leaderboard', leaderboardRoutes);
 
-// Test route
-app.get('/', (req, res) => res.send('Server is running...'));
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, 'dist')));
 
+// For SPA routing (only match non-API routes for SPA support)
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 // Connect to the database
 (async () => {
   await connectDB();
